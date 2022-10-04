af04a3579e8b35002dc953272b686e9adc801874
OlgaMamontova <mamontolly@gmail.com>
2022-10-04 08:18:56 +0300

    added 2 generator functions

---
+++ hw-13(14) Символи. Генератори. Ітератори/main.js
@@ -0,0 +1,46 @@
+// нескінченний генератор ідентифікаторів
+function* createIdGenerator(){
+    for(let i = 1; true; i++){
+        yield i;
+    };
+};
+const idGenerator = createIdGenerator();
+
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+
+//генератор, який буде регулювати розміри шрифту
+let text = document.querySelector('.text');
+document.querySelector('.up').addEventListener('click', function(){
+    text.style.fontSize = fontGenerator.next("up").value + 'px';
+});
+document.querySelector('.down').addEventListener('click', function(){
+    text.style.fontSize = fontGenerator.next("down").value + 'px';
+});
+
+function* newFontGenerator(size){
+    let calcSize;
+    while(true){
+        if (calcSize === 'up') {
+            size += 2;
+        };
+        if (calcSize === 'down') {
+            size -= 2;
+        };
+        calcSize = yield size;
+    };
+};
+const fontGenerator = newFontGenerator(14);
+fontGenerator.next();
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next().value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next().value);
+
